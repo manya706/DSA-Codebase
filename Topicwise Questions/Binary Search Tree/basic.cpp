9875794a8a8f9f2b2e4fe19e9d0d58c2136f5681
manya706 <sharma.manya721@gmail.com>
2023-09-02 10:20:36 +0530

    basic operation in bst

---
+++ Topicwise Questions/Binary Search Tree/basic.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode{
+    int val;
+    struct TreeNode *left, *right;
+};
+struct TreeNode*  newNode(int item){
+    struct TreeNode* temp = (struct TreeNode*)malloc(sizeof(TreeNode));
+    temp->val = item;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+struct TreeNode* insert(struct TreeNode* root,int value){
+    if(root==NULL) return newNode(value);
+    if(value < root->val) root->left = insert(root->left, value);
+    else if(value > root->val) root->right = insert(root->right, value);
+    return root;
+}
+void printLeaves(struct TreeNode* root){
+    if(!root) return;
+    if(!root->left && !root->right) {
+        cout<<" "<<root->val;
+    }
+    if(root->left) printLeaves(root->left);
+    if(root->right) printLeaves(root->right);
+}
+int height(struct TreeNode* node)
+{
+    if (node == NULL)
+        return 0;
+    else {
+  
+        // Compute the depth of each subtree
+        int lDepth = height(node->left);
+        int rDepth = height(node->right);
+  
+        // Use the larger one
+        if (lDepth > rDepth)
+            return (lDepth + 1);
+        else
+            return (rDepth + 1);
+    }
+}
+void levelPrint(struct TreeNode* root, int l){
+    if(!root) return;
+    if(l==1) cout<<root->val<<" ";
+    else if(l>1){
+        levelPrint(root->left, l--);
+        levelPrint(root->right, l--);
+    }
+}
+int main(){
+    struct TreeNode* root = NULL;
+  
+    // Creating the BST
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 70);
+    insert(root, 60);
+    insert(root, 80);
+
+    cout<<height(root)<<endl;
+    levelPrint(root,2);
+    cout<<endl;
+    printLeaves(root);
+}
