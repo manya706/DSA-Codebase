f3ec13fc8b01841299c1c6c30465e39a911354f7
Manya Sharma <96016153+manya706@users.noreply.github.com>
2023-09-28 20:14:01 +0530

    Added solution - LeetHub

---
+++ Coin Change - GFG/coin-change.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long long int solver(int i, int sum, int coins[], vector<vector<long long int>> &dp){
+        if(i==0) return (sum%coins[0]==0);
+        if(dp[i][sum] != -1) return dp[i][sum];
+        long long int nottake = solver(i-1,sum,coins,dp);
+        long long int take =0;
+        if(coins[i]<=sum){
+            take = solver(i,sum-coins[i],coins,dp);
+        }
+        return dp[i][sum] = take+nottake;
+    }
+    long long int count(int coins[], int N, int sum) {
+        vector<vector<long long int>> dp(N, vector<long long int> (sum+1,-1));
+        // code here.
+        return solver(N-1, sum,coins,dp);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int sum, N;
+        cin >> sum >> N;
+        int coins[N];
+        for (int i = 0; i < N; i++) cin >> coins[i];
+        Solution ob;
+        cout << ob.count(coins, N, sum) << endl;
+    }
+
+    return 0;
+}
+
+
+// } Driver Code Ends
