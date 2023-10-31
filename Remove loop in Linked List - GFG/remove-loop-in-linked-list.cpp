50018f2de884959feb4ae150aab13e0af11a2814
Manya Sharma <96016153+manya706@users.noreply.github.com>
2023-10-31 10:45:28 +0530

    Added solution - LeetHub

---
+++ Remove loop in Linked List - GFG/remove-loop-in-linked-list.cpp
@@ -0,0 +1,181 @@
+//{ Driver Code Starts
+// driver code
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+bool isLoop(Node* head)
+{
+    if(!head) return false;
+    
+    Node* fast = head->next;
+    Node* slow = head;
+    
+    while( fast != slow)
+    {
+        if( !fast || !fast->next ) return false;
+        fast=fast->next->next;
+        slow=slow->next;
+    }
+    
+    return true;
+}
+
+int length(Node* head)
+{
+    int ret = 0;
+    while(head)
+    {
+        ret++;
+        head = head->next;
+    }
+    return ret;
+}
+
+bool notOriginal(Node *head, unordered_map<Node *, int>&myMap){
+    
+    while(head){
+        if(myMap.find(head)==myMap.end()) return true;
+        if(myMap[head] != (head->data)) return true;
+        
+        head=head->next;
+    }
+}
+
+
+
+
+// } Driver Code Ends
+/*
+structure of linked list node:
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution
+{
+    public:
+    void removeutil(Node* loop_n, Node* list){
+        Node* ptr2 = loop_n;
+        Node* ptr1 = loop_n;
+        unsigned int k=1,i;
+    while (ptr1->next != ptr2) {
+        ptr1 = ptr1->next;
+        k++;
+    }
+ 
+    // Fix one pointer to head
+    ptr1 = list;
+ 
+    // And the other pointer to k nodes after head
+    ptr2 = list;
+    for (i = 0; i < k; i++)
+        ptr2 = ptr2->next;
+ 
+    /*  Move both pointers at the same pace,
+      they will meet at loop starting node */
+    while (ptr2 != ptr1) {
+        ptr1 = ptr1->next;
+        ptr2 = ptr2->next;
+    }
+ 
+    // Get pointer to the last node
+    while (ptr2->next != ptr1)
+        ptr2 = ptr2->next;
+ 
+    /* Set the next node of the loop ending node
+      to fix the loop */
+    ptr2->next = NULL;
+            
+    }
+    //Function to remove a loop in the linked list.
+    void removeLoop(Node* head)
+    {
+        Node* fast = head;
+        Node* slow = head;
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+            if(slow==fast){
+                removeutil(slow,head);
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        unordered_map<Node *, int>myMap;
+        
+        int n, num;
+        cin>>n;
+        
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+        
+        myMap[head]=num;
+        
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+            myMap[tail]=num;
+        }
+        
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+        
+        Solution ob;
+        ob.removeLoop(head);
+        
+        if( isLoop(head) || length(head)!=n || notOriginal(head, myMap))
+            cout<<"0\n";
+        else
+            cout<<"1\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
