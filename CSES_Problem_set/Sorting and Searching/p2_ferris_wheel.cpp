a04fdfcf94db01cb4ff0e143ad7c09ae749c01df
manya706 <sharma.manya721@gmail.com>
2024-01-17 00:01:28 +0530

    working on p2

---
+++ CSES_Problem_set/Sorting and Searching/p2_ferris_wheel.cpp
@@ -0,0 +1,25 @@
+// There are n children who want to go to a Ferris wheel, and your task is to find a gondola for each child.
+// Each gondola may have one or two children in it, and in addition, the total weight in a gondola may not exceed x. You know the weight of every child.
+// What is the minimum number of gondolas needed for the children?
+// Input
+// The first input line contains two integers n and x: the number of children and the maximum allowed weight.
+// The next line contains n integers p_1,p_2,\ldots,p_n: the weight of each child.
+// Output
+// Print one integer: the minimum number of gondolas.
+
+
+// Example
+// Input:
+// 4 10
+// 7 2 3 9
+
+// Output:
+// 3
+#include<bits/stdc++.h>
+#include<vector>
+#include<math.h>
+#include<map>
+using namespace std;
+typedef long long int ll;
+
+
