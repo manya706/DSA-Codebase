0859a4fa5d834ac1e40e0b43b1854eb8b3bac5d9
manya706 <sharma.manya721@gmail.com>
2024-03-24 22:12:44 +0530

    accepted

--- TLE sheet/800/Sequence Game.cpp
+++ TLE sheet/800/Sequence Game.cpp
@@ -3,3 +3,33 @@ using namespace std;
 typedef vector<int> vi;
 #define pb push_back
 #define ll long long int
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vi arr;
+        for(int i=0;i<n;i++){
+            int x;
+            cin>>x;
+            arr.pb(x);
+        }
+        vi ans;
+        ans.pb(arr[0]);
+        for(int i=1;i<n;i++){
+            if(arr[i]>=arr[i-1]){
+                ans.pb(arr[i]);
+            }
+            else{
+                ans.pb(arr[i]);
+                ans.pb(arr[i]);
+            }
+        }
+        cout<<ans.size()<<endl;
+        for(int i=0;i<ans.size();i++){
+            cout<<ans[i]<<" ";
+        }
+        cout<<endl;
+    }
+}
