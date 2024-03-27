894112f9cd964e68b7bef0288b7e4bbb527bd6ac
manya706 <sharma.manya721@gmail.com>
2024-03-27 23:55:26 +0530

    start

---
+++ TLE_sheet/800/Forbidden Integer.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+#define pb push_back
+#define ll long long int
+#define sz(x)             int ((x).size())
+#define All(x)            (x).begin(), (x).end()
+#define scanv(x)          for(auto &i: x) cin >> i
