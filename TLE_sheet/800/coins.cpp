0fed1df4abc15863d1bd35f07c354a7957629b8d
manya706 <sharma.manya721@gmail.com>
2024-03-30 17:26:34 +0530

    accepted  --maths

---
+++ TLE_sheet/800/coins.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+#define pb push_back
+#define ll long long int
+#define sz(x)             int ((x).size())
+#define All(x)            (x).begin(), (x).end()
+#define scanv(x)          for(auto &i: x) cin >> i
+// 2⋅x+k⋅y=n
+void solve(){
+    int n,k;
+    cin>>n>>k;
+    if(n%2 && (k%2==0)){
+        cout<<"NO"<<endl;
+    }
+    else cout<<"YES"<<endl;
+
+    
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
