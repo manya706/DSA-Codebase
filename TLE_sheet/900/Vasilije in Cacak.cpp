6b6282fdaffa69c8030a0c7c9aa326e782c53dd9
manya706 <sharma.manya721@gmail.com>
2024-04-08 00:03:27 +0530

    accepted

---
+++ TLE_sheet/900/Vasilije in Cacak.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define ll long long int
+#define sz(x)             int ((x).size())
+#define All(x)            (x).begin(), (x).end()
+#define scanv(x)          for(auto &i: x) cin >> i
+#define mk make_pair
+#define pii pair<int, int>
+#define a first
+#define b second
+#define vi vector<int>
+#define umap unordered_map
+#define uset unordered_set
+#define MOD 1000000007
+ll gcd(ll a, ll b)              { if (b == 0) return a; return gcd(b, a % b); }
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+#define neg1                cout<<-1<<endl
+#define imax INT_MAX
+#define imin INT_MIN
+#define exp 1e9
+void solve(){
+    ll n,k,x;
+    cin>>n>>k>>x;
+    ll max = (n*(n+1) - (n-k)*(n-k+1))/2; //n=10, k=3, x=10, 110-56=54
+    ll min = (k*(k+1))/2;
+    if(x<=max && x>=min) yes;
+    else no;
+
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int ttt; cin >> ttt;
+    while(ttt--) {
+        solve();
+    }
+    return 0;
+}
