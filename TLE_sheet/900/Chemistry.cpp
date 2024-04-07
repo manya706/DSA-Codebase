6d9d4b564c9fa6e64711d6bc814cbda9a58ccc76
manya706 <sharma.manya721@gmail.com>
2024-04-07 23:32:40 +0530

    accepted

---
+++ TLE_sheet/900/Chemistry.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define ll long long int
+#define sz(x)             int ((x).size())
+#define All(x)            (x).begin(), (x).end()
+#define scanv(x)          for(auto &i: x) cin >> i
+#define mk make_pair
+#define pii pair<int, int>
+#define a first
+#define b second
+#define vi vector<int>
+#define umap unordered_map
+#define uset unordered_set
+#define MOD 1000000007
+ll gcd(ll a, ll b)              { if (b == 0) return a; return gcd(b, a % b); }
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+#define neg1                cout<<-1<<endl
+#define imax INT_MAX
+#define imin INT_MIN
+#define exp 1e9
+void solve(){
+    int n,k;
+    cin>>n>>k;
+    string s;
+    cin>>s;
+    unordered_map<char,int> mp;
+    for(char i:s){
+        mp[i]++;
+    }
+    int o=0;
+    for(auto i:mp){
+        if(i.second%2){
+            o++;
+        }
+    }
+    if(n-k==0 || o>k+1) no;
+    else yes;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int ttt; cin >> ttt;
+    while(ttt--) {
+        solve();
+    }
+    return 0;
+}
