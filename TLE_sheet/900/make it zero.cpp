80a88de57305625a86e9508267697fe2082cdc46
manya706 <sharma.manya721@gmail.com>
2024-04-09 23:18:32 +0530

    accepted

---
+++ TLE_sheet/900/make it zero.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define ll long long int
+#define sz(x)             int ((x).size())
+#define All(x)            (x).begin(), (x).end()
+#define scanv(x)          for(auto &i: x) cin >> i
+#define mk make_pair
+#define pii pair<int, int>
+#define a first
+#define b second
+#define vi vector<int>
+#define umap unordered_map
+#define uset unordered_set
+#define MOD 1000000007
+ll gcd(ll a, ll b)              { if (b == 0) return a; return gcd(b, a % b); }
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+#define neg1                cout<<-1<<endl
+#define imax INT_MAX
+#define imin INT_MIN
+#define exp 1e9
+void solve(){
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    bool f=false;
+    int flag=1;
+    for(auto &x:v)
+    {
+        cin>>x;
+        if(x!=0)
+            flag=0;
+    }
+    if(f){
+        cout<<0<<endl;
+        return;
+    } 
+    if(n%2){
+        cout<<4<<endl;
+        cout<<1<<" "<<n-1<<endl;
+        cout<<1<<" "<<n-1<<endl;
+        cout<<n-1<<" "<<n<<endl;
+        cout<<n-1<<" "<<n<<endl;
+    }
+    else{
+        cout<<2<<endl;
+        cout<<1<<" "<<n<<endl;
+        cout<<1<<" "<<n<<endl;
+    }
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int ttt; cin >> ttt;
+    while(ttt--) {
+        solve();
+    }
+    return 0;
+}
