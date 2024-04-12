c1c70a43431b1c511947e532e7abe48e9dabe8bd
manya706 <sharma.manya721@gmail.com>
2024-04-12 22:37:48 +0530

    accepted

---
+++ TLE_sheet/900/Mainak and Array.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define ll long long int
+#define sz(x)             int ((x).size())
+#define All(x)            (x).begin(), (x).end()
+#define scanv(x)          for(auto &i: x) cin >> i
+#define mk make_pair
+#define pii pair<int, int>
+#define a first
+#define b second
+#define vi vector<int>
+#define umap unordered_map
+#define uset unordered_set
+#define MOD 1000000007
+ll gcd(ll a, ll b)              { if (b == 0) return a; return gcd(b, a % b); }
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+#define neg1                cout<<-1<<endl
+#define imax INT_MAX
+#define imin INT_MIN
+#define exp 1e9
+void solve(){
+    int n;
+    cin>>n;
+    vector<int> a;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        a.pb(x);
+    }
+    if(n==1) {
+        cout<<0<<endl;
+        return;
+    }
+    int mx=INT_MIN;
+    for(int i=1;i<n;i++){
+        mx = max(mx, a[i-1]-a[i]);
+    }
+    for(int i=1;i<n;i++){
+        mx = max(mx,a[i]-a[0]);
+    }
+    for(int i=0;i<n-1;i++){
+        mx = max(mx,a[n-1]-a[i]);
+    }
+    cout<<mx<<endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int ttt; cin >> ttt;
+    while(ttt--) {
+        solve();
+    }
+    return 0;
+}
