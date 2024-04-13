e60f4c0aeb44f14ac0b9f7cc33db49bdce13e229
manya706 <sharma.manya721@gmail.com>
2024-04-13 10:25:10 +0530

    accepted

---
+++ TLE_sheet/900/AvtoBus.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define ll long long int
+#define sz(x)             int ((x).size())
+#define All(x)            (x).begin(), (x).end()
+#define scanv(x)          for(auto &i: x) cin >> i
+#define mk make_pair
+#define pii pair<int, int>
+#define a first
+#define b second
+#define vi vector<int>
+#define umap unordered_map
+#define uset unordered_set
+#define MOD 1000000007
+ll gcd(ll a, ll b)              { if (b == 0) return a; return gcd(b, a % b); }
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+#define neg1                cout<<-1<<endl
+#define imax INT_MAX
+#define imin INT_MIN
+#define exp 1e9
+void solve(){
+    ll n;
+    cin>>n;
+    if(n<4 || n&1) {
+        neg1;
+        return;
+    }
+    ll m4 = n/4;
+    ll m6 = n/6;
+    if(n%6) m6++;
+    cout<<m6<<" "<<m4<<endl;
+
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int ttt; cin >> ttt;
+    while(ttt--) {
+        solve();
+    }
+    return 0;
+}
