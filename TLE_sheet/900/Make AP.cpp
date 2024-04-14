a4481b3cddd96e4c92b926be5ff923342eeecc2b
manya706 <sharma.manya721@gmail.com>
2024-04-14 12:27:04 +0530

    accepted

---
+++ TLE_sheet/900/Make AP.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define ll long long int
+#define sz(x)             int ((x).size())
+#define All(x)            (x).begin(), (x).end()
+#define scanv(x)          for(auto &i: x) cin >> i
+#define mk make_pair
+#define pii pair<int, int>
+#define a first
+#define b second
+#define vi vector<int>
+#define umap unordered_map
+#define uset unordered_set
+#define MOD 1000000007
+ll gcd(ll a, ll b)              { if (b == 0) return a; return gcd(b, a % b); }
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+#define neg1                cout<<-1<<endl
+#define imax INT_MAX
+#define imin INT_MIN
+#define exp 1e9
+void solve(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    if((2*b-c)>0 && (2*b-c)%a==0)
+        cout << "YES" << endl;
+    else if((a+c)%(2*b)==0)
+        cout << "YES" << endl;  
+    else if((2*b-a)>0 && (2*b-a)%c==0)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int ttt; cin >> ttt;
+    while(ttt--) {
+        solve();
+    }
+    return 0;
+}
