c3bcff7558a2c8cf6786e48e28028bbe64600c71
manya706 <sharma.manya721@gmail.com>
2024-04-18 11:13:55 +0530

    accepted

---
+++ TLE_sheet/900/Strange Partition.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define ll long long int
+#define sz(x)             int ((x).size())
+#define All(x)            (x).begin(), (x).end()
+#define scanv(x)          for(auto &i: x) cin >> i
+#define mk make_pair
+#define pii pair<int, int>
+#define a first
+#define b second
+#define vi vector<int>
+#define umap unordered_map
+#define uset unordered_set
+#define MOD 1000000007
+ll gcd(ll a, ll b)              { if (b == 0) return a; return gcd(b, a % b); }
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+#define neg1                cout<<-1<<endl
+#define imax INT_MAX
+#define imin INT_MIN
+#define exp 1e9
+void solve(){
+    ll n,x;
+    cin>>n>>x;
+    ll sum=0, sum_fr=0;
+    for(int i=0;i<n;i++){
+        ll t;
+        cin>>t;
+        sum+=t;
+        sum_fr += ((t/x));
+        if(t%x!=0) sum_fr++;
+    }
+    ll r = sum/x;
+    if(sum%x!=0){
+        r++;
+    }
+    cout<<r<<" "<<sum_fr<<endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int ttt; cin >> ttt;
+    while(ttt--) {
+        solve();
+    }
+    return 0;
+}
