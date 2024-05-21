5361e43547336c692a8dd4b8827281e8e1977615
Manya Sharma <96016153+manya706@users.noreply.github.com>
2024-05-21 14:56:20 +0530

    Time: 2 ms (43.40%) | Memory: 9.6 MB (94.96%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -5,6 +5,29 @@ public:
         //  this is like sorting 0,1,2
         // can using direct sort vector methos
         //  or use two pointers
-        sort(nums.begin(),nums.end());
+        // sort(nums.begin(),nums.end());
+    //    lets just count number of 0,1 and 2
+        int cnt0=0, cnt1=0, cnt2=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0) cnt0++;
+            if(nums[i]==2) cnt2++;
+            if(nums[i]==1) cnt1++;
+        }
+        int i=0;
+        while(cnt0!=0){
+            nums[i] = 0;
+            cnt0--;
+            i++;
+        }
+        while(cnt1!=0){
+            nums[i]=1;
+            cnt1--;
+            i++;
+        }
+        while(cnt2 != 0){
+            nums[i] =2;
+            cnt2--;
+            i++;
+        }
     }
 };
