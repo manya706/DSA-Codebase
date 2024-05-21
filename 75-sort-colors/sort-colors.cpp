7cd3c321d0671ddf43fa7c119098a0622ae3892a
Manya Sharma <96016153+manya706@users.noreply.github.com>
2024-05-21 14:51:00 +0530

    Time: 5 ms (17.42%) | Memory: 9.8 MB (71.47%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+
+    void sortColors(vector<int>& nums) {
+        //  this is like sorting 0,1,2
+        // can using direct sort vector methos
+        //  or use two pointers
+        sort(nums.begin(),nums.end());
+    }
+};
