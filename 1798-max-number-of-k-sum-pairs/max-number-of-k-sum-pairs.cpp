cc4d1b0510daa6f05a532f1662f0830046d8f45c
Manya Sharma <96016153+manya706@users.noreply.github.com>
2024-05-28 00:55:23 +0530

    Time: 54 ms (98.99%) | Memory: 62.4 MB (38.16%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
         // hash table or two-pointers
-        ios_base ::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+        ios_base::sync_with_stdio(false);
         int n = nums.size();
         int s=0, e= n-1;
         sort(nums.begin(),nums.end());
