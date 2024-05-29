ae76e9750586f41adb2616ff673b95f685a0ee9e
Manya Sharma <96016153+manya706@users.noreply.github.com>
2024-05-29 21:51:27 +0530

    Time: 25 ms (98.54%) | Memory: 57.8 MB (48.54%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
+        ios_base :: sync_with_stdio(false), cin.tie(0), cout.tie(0);
         int i=0, j=0, ans=0;
         int cost =0, n=nums.size();
         while(j<n){
