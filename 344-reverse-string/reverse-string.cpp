4e28fc30fc3069934427b105b567f62d6d442aad
Manya Sharma <96016153+manya706@users.noreply.github.com>
2024-06-02 22:44:50 +0530

    Time: 20 ms (39.08%) | Memory: 26.8 MB (89.96%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -4,11 +4,13 @@ public:
         // will try using two pointers 
         int l=0, r=s.size()-1;
         while(l<=r){
-            char temp = s[l];
-            s[l] = s[r];
-            s[r] = temp;
+            // char temp = s[l];
+            // s[l] = s[r];
+            // s[r] = temp;
+            swap(s[l], s[r]);
             l++;
             r--;
+            
         }
     }
 };
