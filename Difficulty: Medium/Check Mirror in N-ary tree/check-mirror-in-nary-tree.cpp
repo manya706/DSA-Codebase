acdc05d2eb1504f13b066fe4272ecdb3f4538343
Manya Sharma <96016153+manya706@users.noreply.github.com>
2024-08-24 18:02:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Check Mirror in N-ary tree/check-mirror-in-nary-tree.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int checkMirrorTree(int n, int e, int A[], int B[]) {
+        // code here
+        unordered_map<int, stack<int>> mp;
+        for(int i=0;i<2*e;i+=2){
+            mp[A[i]].push(A[i+1]);
+        }
+        for(int i=0;i<2*e;i+=2){
+            if(mp[B[i]].top()==B[i+1]){
+                mp[B[i]].pop();
+            }
+            else return 0;
+        }
+        return 1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n,e;
+        
+        cin>>n>>e;
+        int A[2*e], B[2*e];
+        
+        for(int i=0; i<2*e; i++)
+            cin>>A[i];
+            
+        for(int i=0; i<2*e; i++)
+            cin>>B[i];
+
+        Solution ob;
+        cout << ob.checkMirrorTree(n,e,A,B) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
