c4d272b59d0cd574ee21d67e90c4bec4cc5e9879
Manya Sharma <96016153+manya706@users.noreply.github.com>
2024-08-27 19:23:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate by 90 degree/rotate-by-90-degree.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+void rotate (vector<vector<int> >& matrix);
+
+
+// } Driver Code Ends
+//User function template for C++
+
+/* matrix : given input matrix, you are require 
+ to change it in place without using extra space */
+void rotate(vector<vector<int> >& matrix)
+{
+    // Your code goes here
+    int n = matrix.size();
+    int m = matrix[0].size();
+    //transpose of matrix
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            swap(matrix[i][j],matrix[j][i]);
+        }
+    }
+    
+    for(int i=0;i<m;i++){
+        for(int j=0;j<m/2;j++){
+            swap(matrix[j][i], matrix[n-j-1][i]);
+        }
+    }
+    
+}
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t; 
+    while(t--)
+    {
+        int n;
+        cin>>n; 
+        vector<vector<int> > matrix(n);
+        for(int i=0; i<n; i++)
+        {
+            matrix[i].resize(n);
+            for(int j=0; j<n; j++)
+                cin>>matrix[i][j]; 
+        }
+        rotate(matrix);
+        for (int i = 0; i < n; ++i)
+        {
+            for(int j=0; j<n; j++)
+                cout<<matrix[i][j]<<" ";
+            cout<<"\n";
+        }
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
