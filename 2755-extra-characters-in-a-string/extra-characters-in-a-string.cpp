2025df37db9728b7c1cf6171e57a6c66de58c1ee
Manya Sharma <96016153+manya706@users.noreply.github.com>
2024-09-23 13:02:04 +0530

    Time: 150 ms (42.91%) | Memory: 86.8 MB (52.70%) - LeetSync

---
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minExtraChar(string s, vector<string>& dictionary) {
+        unordered_set<string> dict(dictionary.begin(), dictionary.end());
+        int n = s.length();
+        vector<int> dp(n+1, 0);
+        for(int i=n-1 ; i>=0 ; --i){
+            dp[i] = dp[i+1]+1;
+            for(int j = 1; j<=n-i ; ++j){
+                string sub = s.substr(i,j);
+                if(dict.find(sub)!=dict.end()){
+                    dp[i] = min(dp[i], dp[i+j]);
+                }
+            }
+        }
+        return dp[0];
+
+    }
+};
