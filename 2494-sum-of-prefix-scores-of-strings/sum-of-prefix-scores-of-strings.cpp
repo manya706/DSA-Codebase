f054e3450bbbcb41ebcc4fab5c9238544344517b
Manya Sharma <96016153+manya706@users.noreply.github.com>
2024-09-25 17:28:04 +0530

    Time: 522 ms (82.79%) | Memory: 725.9 MB (17.45%) - LeetSync

--- 2494-sum-of-prefix-scores-of-strings/sum-of-prefix-scores-of-strings.cpp
+++ 2494-sum-of-prefix-scores-of-strings/sum-of-prefix-scores-of-strings.cpp
@@ -57,3 +57,9 @@ public:
         return res;
     }
 };
+static const int MS = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
