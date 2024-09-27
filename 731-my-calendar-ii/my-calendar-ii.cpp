4703a9797aa1c881a28d029ce1a1200e3dee6078
Manya Sharma <96016153+manya706@users.noreply.github.com>
2024-09-27 14:58:33 +0530

    Time: 73 ms (88.84%) | Memory: 37.5 MB (99.58%) - LeetSync

---
+++ 731-my-calendar-ii/my-calendar-ii.cpp
@@ -0,0 +1,39 @@
+class MyCalendarTwo {
+public:
+    vector<pair<int,int>> booking;
+    MyCalendarTwo() {}
+    
+    bool book(int x, int y) {
+        for(auto& b: booking){
+            int a = b.first, b2 = b.second;
+            if(x<b2 && y>a){
+                int newx = max(a,x);
+                int newy = min(b2,y);
+                if(doublebooking(newx,newy)){
+                    return false;
+                }
+            }
+            
+        }
+        booking.push_back({x,y});
+        return true;
+    }
+
+    bool doublebooking(int x, int y){
+        int over = 0;
+        for(int i=0;i<booking.size();i++){
+            int a = booking[i].first, b = booking[i].second;
+            if(x<b and y>a) {
+                over++;
+                if(over==2) return true;
+            }
+        }
+        return false;
+    }
+};
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool param_1 = obj->book(start,end);
+ */
