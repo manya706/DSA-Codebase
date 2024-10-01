b1bdcb69c39827408fb10b4ac6cb37f5592849ea
Manya Sharma <96016153+manya706@users.noreply.github.com>
2024-10-01 14:12:02 +0530

    Time: 81 ms (87.96%) | Memory: 64.4 MB (76.85%) - LeetSync

---
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+        vector<int> freq(k,0);
+        for(int n: arr){
+            int rem = n%k;
+            if(rem<0) rem += k;
+            freq[rem] ++;
+        }
+        if(freq[0]%2!=0) return false;
+        for(int i=1; i<=k/2; i++){
+            if(freq[i]!=freq[k-i]) return false;
+        }
+        return true;
+    }
+};
