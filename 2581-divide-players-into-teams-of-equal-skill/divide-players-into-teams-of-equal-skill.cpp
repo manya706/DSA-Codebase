903721ee1f9790f319a6f77e4825c73860f42372
Manya Sharma <96016153+manya706@users.noreply.github.com>
2024-10-04 12:53:07 +0530

    Time: 50 ms (96.60%) | Memory: 56.5 MB (56.40%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
@@ -12,3 +12,9 @@ public:
         return chem;
     }
 };
+static const auto ms = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
