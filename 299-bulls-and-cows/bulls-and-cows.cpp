cbd45282e8080405882f0421b6f018cfe2d35a87
Manya Sharma <96016153+manya706@users.noreply.github.com>
2024-10-05 14:47:30 +0530

    Time: 2 ms (77.32%) | Memory: 8.4 MB (87.38%) - LeetSync

---
+++ 299-bulls-and-cows/bulls-and-cows.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        int bulls=0, cows=0;
+        vector<int> s(10,0), g(10,0);
+        for(int i=0;i<secret.size();i++){
+            if(secret[i]==guess[i]) bulls++;
+            else {
+                s[secret[i]-'0']++;
+                g[guess[i]-'0']++;
+            }
+        }
+        for(int i=0;i<10;i++){
+            cows+=min(s[i],g[i]);
+        }
+        string ans = to_string(bulls)+'A'+to_string(cows)+'B';
+        return ans;
+    }
+};
