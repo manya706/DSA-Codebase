5aa22fe96aa73d193831200b7c4c598b88107126
Manya Sharma <96016153+manya706@users.noreply.github.com>
2024-10-14 08:18:07 +0530

    Time: 725 ms (5.24%) | Memory: 79.8 MB (21.70%) - LeetSync

---
+++ 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long maxKelements(vector<int>& nums, int k) {
+        int n = nums.size();
+        long long ans=0;
+        priority_queue<int> pq;
+        for(auto i:nums){
+            pq.push(i);
+        }
+        while(k>0){
+            ans += pq.top();
+            cout<<pq.top()<<" ";
+            int t = pq.top();
+            pq.pop();
+            pq.push((t+3-1)/3);
+            cout<<t/3<<" ";
+            k--;
+        }
+        return ans;
+    }
+};
