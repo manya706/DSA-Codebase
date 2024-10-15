0c361c5cb29661e06c82827739db4da5f4dd392c
Manya Sharma <96016153+manya706@users.noreply.github.com>
2024-10-16 00:55:47 +0530

    Time: 8 ms (38.37%) | Memory: 10.4 MB (50.36%) - LeetSync

---
+++ 38-count-and-say/count-and-say.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n==1) return "1";
+        string s=countAndSay(n-1);
+        string ans="";
+        for(int i=0;i<s.length();i++){
+            char c = s[i];
+            int cnt=1;
+            while(i<s.length()-1 && s[i]==s[i+1]){
+                cnt++;
+                i++;
+            }
+            ans+=to_string(cnt)+string(1,c); // to convert char to string[string(1,c)]
+        }
+        return ans;
+    }
+};
