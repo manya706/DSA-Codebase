3e5fc3e3be4f80b838eebdcb672e4a32bd69b578
Manya Sharma <96016153+manya706@users.noreply.github.com>
2024-11-01 15:10:51 +0530

    Time: 31 ms (42.86%) | Memory: 42.7 MB (63.26%) - LeetSync

---
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string makeFancyString(string s) {
+        string ans = "";
+        int n = s.length();
+        int c =0;
+        ans.push_back(s[0]);
+        c++;
+        for(int i=1;i<n;i++){
+            if(s[i]==ans.back()){
+                c++;
+                if(c<3) ans.push_back(s[i]);
+            }
+            else{
+                c = 1;
+                ans.push_back(s[i]);
+            }
+        }
+        return ans;
+    }
+};
