24ee604b10051ff80001ba7604f7329fff66e9e8
Manya Sharma <96016153+manya706@users.noreply.github.com>
2024-11-27 11:49:28 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (5.95%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        // using binary search
+        int n = nums.size();
+        int l=0, r=n-1;
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[mid]>=nums[l]) {
+                if(nums[l]<=target && target<nums[mid]){
+                    r = mid-1;
+                } else {
+                    l = mid+1;
+                    } 
+                }
+                else {
+                    if(nums[mid]<target && target<=nums[r]){
+                        l = mid+1;
+                    } else {
+                        r = mid-1;
+                    }
+                
+            }
+        }
+        return -1;
+    }
+};
